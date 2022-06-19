08c37da2f830d56fd3e75f5017b9d164074235f4
aguselnikov <aguselnikov@fil-it.ru>
2022-06-20 00:09:50 +0300

    add module swagger

---
+++ swagger/swagger/src/main/java/com/theforest/swagger/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.theforest.swagger.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Operation(summary = "Get message \"hello\"")
+    @ApiResponse(responseCode = "200", description = "get message hello")
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
