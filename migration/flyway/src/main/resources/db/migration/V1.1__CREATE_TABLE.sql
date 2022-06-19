e0caa8bb809eb9142f77ae750ada99cf98cb71c5
aguselnikov <aguselnikov@fil-it.ru>
2022-06-19 21:56:09 +0300

    add module migration [flyway]

---
+++ migration/flyway/src/main/resources/db/migration/V1.1__CREATE_TABLE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE COMPANY.COMPANY(
+                    	ID INTEGER NOT NULL,
+                    	TITLE VARCHAR(255),
+                    	DESCRIPTION VARCHAR(255),
+                    	PRIMARY KEY (ID)
+                    );
