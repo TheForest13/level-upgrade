ea5b7434d3c59368025271dcd83e069aafca071a
TheForest <without_mail>
2024-10-13 13:53:06 +0300

    add metric module

---
+++ metric/micrometer/src/main/java/com/theforest/micrometer/CustomController.java
@@ -0,0 +1,54 @@
+package com.theforest.micrometer;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@RestController
+public class CustomController {
+
+
+    private CacheManager cacheManager;
+
+    public CustomController(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @CachePut(cacheNames="msg", value="msg")
+    @PostMapping("/send")
+    public String send(@RequestBody String msg) {
+        log.info("send msg: {}", msg);
+        return msg;
+    }
+
+    @GetMapping()
+    public List<CacheInfoDto> getCacheInfo() {
+        return cacheManager.getCacheNames()
+                .stream()
+                .map(this::getCacheInfo)
+                .toList();
+    }
+
+    private CacheInfoDto getCacheInfo(String cacheName) {
+        Cache<Object, Object> nativeCache =
+                (Cache)cacheManager.getCache(cacheName).getNativeCache();
+        Set<Object> keys = nativeCache.asMap().keySet();
+        CacheStats stats = nativeCache.stats();
+        return new CacheInfoDto(
+                cacheName, keys.size(), keys, stats.toString());
+    }
+
+    private record CacheInfoDto(
+            String name, int size, Set<Object> keys, String stats) {}
+
+}
